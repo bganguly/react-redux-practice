9e4d349033b5b34876b10199fc714574192d4e61
bganguly <gangulybikramjit@hotmail.com>
2022-06-05 13:15:34 -0700

    applied css to make elemenets appear side by side

--- src/components/ParentChildProps.js
+++ src/components/ParentChildProps.js
@@ -2,16 +2,18 @@ import { useState } from "react"
 import CustomInput from "./CustomInput"
 import CustomPara from "./CustomPara"
 
+import  "./ParentChildProps.css"
+
 const ParentChildProps = () => {
   const [name, setName] = useState('')
   const onChange = (e) => {
     setName(e.target.value)
   }
   return (
-    <>
+    <div className="sideBySide">
       <CustomInput onChange={onChange}/>    
       <CustomPara name={name}/>
-    </>
+    </div>
   )
 }
 
