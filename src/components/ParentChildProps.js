fb07d72effa13e0e9bfab4d8ed48c9def70e7c88
bganguly <gangulybikramjit@hotmail.com>
2022-06-04 21:07:23 -0700

    demo of passing state between sibling

--- src/components/ParentChildProps.js
+++ src/components/ParentChildProps.js
@@ -1,12 +1,13 @@
 import { useState } from "react"
 import CustomInput from "./CustomInput"
+import CustomPara from "./CustomPara"
 
 const ParentChildProps = () => {
   const [name, setName] = useState('')
   return (
     <>
       <CustomInput onChange={(e) => {setName(e.target.value)}}/>    
-      <p>You entered : {name}</p>  
+      <CustomPara name={name}/>
     </>
   )
 }
